71240b787dbf2e6e01af34c6de6769be598591c4
Carlos Monzalvo N <carlos.monzalvo.n@gmail.com>
2019-10-06 20:43:32 -0500

    agrego animacion json con timer y tab bar

---
+++ Cine/AnimationViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AnimationViewController.swift
+//  Cine
+//
+//  Created by Pedro Carlos Monzalvo Navarro on 10/5/19.
+//  Copyright © 2019 Pedro Carlos Monzalvo Navarro. All rights reserved.
+//
+
+import UIKit
+import Lottie
+
+class AnimationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        animation()
+        Timer.scheduledTimer(withTimeInterval: 5.0, repeats: false) { (Timer) in
+            self.performSegue(withIdentifier: "firstView", sender: nil)
+        }
+    }
+    func animation(){
+        let animationView = AnimationView(name: "cinema-animation")
+        animationView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        animationView.center = self.view.center
+        animationView.contentMode = .scaleAspectFill
+        
+        view?.addSubview(animationView)
+        animationView.loopMode = .loop
+        animationView.play()
+    }
+
+
+}
